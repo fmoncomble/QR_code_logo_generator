7329e669805f53a7178b77b8071ad3be2916dcff
fmoncomble <florent.moncomble@gmail.com>
2023-09-02 17:12:01 +0200

    Update to v0.2

--- firefox/popup.js
+++ firefox/popup.js
@@ -1,20 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const openQrCodeButton = document.getElementById("openQrCodeButton");
+	browser.runtime.sendMessage({ action: 'generateUrl' }, function (response) {
+		if (browser.runtime.lastError) {
+			console.error(browser.runtime.lastError);
+			return;
+		}
 
-    openQrCodeButton.addEventListener("click", function () {
-        browser.runtime.sendMessage({ action: 'generateUrl' }, function (response) {
-            if (browser.runtime.lastError) {
-                console.error(browser.runtime.lastError);
-                return;
-            }
+		if (response && response.url) {
+			const url = response.url;
+			console.log('URL:', url);
+			
+			const qrCodeContainer = document.getElementById('qr-code-container');
 
-            if (response && response.url) {
-                const url = response.url;
-                const qrCodePageUrl = browser.runtime.getURL("qr_code.html") + "?data=" + encodeURIComponent(url);
-                browser.tabs.create({ url: qrCodePageUrl });
-            } else {
-                console.error('Response is undefined or missing URL');
-            }
-        });
-    });
+			const qrCode = new QRCodeStyling({
+				height: 180,
+				width: 180,
+				data: url
+			});
+
+			qrCode.append(qrCodeContainer);
+			
+			const downloadButton = document.getElementById("downloadButton");
+			downloadButton.addEventListener("click", function () {
+				if (!qrCode) {
+					console.error('No QR Code to download');
+					return;
+				} else {
+					qrCode.download({ name: 'qrcode', extension: 'png'});
+				}
+			});	
+			
+			const customiseButton = document.getElementById("customiseButton");
+			customiseButton.addEventListener("click", function () {
+				if (!url) {
+					console.error('URL not defined');
+					return;
+				} else {
+					const qrCodePageUrl = browser.runtime.getURL("qr_code.html") + "?data=" + encodeURIComponent(url);
+					browser.tabs.create({ url: qrCodePageUrl });
+				}
+			});	
+			
+		} else {
+			console.error('Response is undefined or missing URL');
+		};
+		
+	});
 });
