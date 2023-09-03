ce90c89061b8453b067c30af3b84858cd7ef7b39
fmoncomble <florent.moncomble@gmail.com>
2023-09-03 20:13:54 +0200

    Update to v2.2
    Add support for tab icons.
    Minor tweak to options.

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,48 +1,55 @@
-document.addEventListener("DOMContentLoaded", function () {
-	chrome.runtime.sendMessage({ action: 'generateUrl' }, function (response) {
-		if (chrome.runtime.lastError) {
-			console.error(chrome.runtime.lastError);
-			return;
-		}
+document.addEventListener("DOMContentLoaded", function() {
+  chrome.runtime.sendMessage({
+    action: 'generateUrl'
+  }, function(response) {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+      return;
+    }
 
-		if (response && response.url) {
-			const url = response.url;
-			console.log('URL:', url);
-			
-			const qrCodeContainer = document.getElementById('qr-code-container');
+    if (response && response.url) {
+      const url = response.url;
+      console.log('URL:', url);
 
-			const qrCode = new QRCodeStyling({
-				height: 180,
-				width: 180,
-				data: url
-			});
+      const qrCodeContainer = document.getElementById('qr-code-container');
 
-			qrCode.append(qrCodeContainer);
-			
-			const downloadButton = document.getElementById("downloadButton");
-			downloadButton.addEventListener("click", function () {
-				if (!qrCode) {
-					console.error('No QR Code to download');
-					return;
-				} else {
-					qrCode.download({ name: 'qrcode', extension: 'png'});
-				}
-			});	
-			
-			const customiseButton = document.getElementById("customiseButton");
-			customiseButton.addEventListener("click", function () {
-				if (!url) {
-					console.error('URL not defined');
-					return;
-				} else {
-					const qrCodePageUrl = chrome.runtime.getURL("qr_code.html") + "?data=" + encodeURIComponent(url);
-					chrome.tabs.create({ url: qrCodePageUrl });
-				}
-			});	
-			
-		} else {
-			console.error('Response is undefined or missing URL');
-		};
-		
-	});
+      const qrCode = new QRCodeStyling({
+        height: 180,
+        width: 180,
+        data: url
+      });
+
+      qrCode.append(qrCodeContainer);
+
+      const downloadButton = document.getElementById("downloadButton");
+      downloadButton.addEventListener("click", function() {
+        if (!qrCode) {
+          console.error('No QR Code to download');
+          return;
+        } else {
+          qrCode.download({
+            name: 'qrcode',
+            extension: 'png'
+          });
+        }
+      });
+
+      const customiseButton = document.getElementById("customiseButton");
+      customiseButton.addEventListener("click", function() {
+        if (!url) {
+          console.error('URL not defined');
+          return;
+        } else {
+          const qrCodePageUrl = chrome.runtime.getURL("qr_code.html") + "?data=" + encodeURIComponent(url);
+          chrome.tabs.create({
+            url: qrCodePageUrl
+          });
+        }
+      });
+
+    } else {
+      console.error('Response is undefined or missing URL');
+    };
+
+  });
 });
--- firefox/qr_code.js
+++ firefox/qr_code.js
@@ -1,215 +1,257 @@
-document.addEventListener("DOMContentLoaded", async function () {
-    const qrCodeContainer = document.getElementById("qr-code-container");
-    const dotColor = document.getElementById("dotColor");
-    const dotType = document.getElementById("dotType");
-    const cornersSquareColor = document.getElementById("cornersSquareColor");
-    const cornersSquareType = document.getElementById("cornersSquareType");
-    const cornersDotColor = document.getElementById("cornersDotColor");
-    const cornersDotType = document.getElementById("cornersDotType");
-    const bgColor = document.getElementById("bgColor");
-    const imageUploadInput = document.getElementById("imageUpload");
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const qrData = urlParams.get("data");
-    const removeButtonContainer = document.getElementById("removeButtonContainer");
-    const loadLogoContainer = document.getElementById("loadLogoContainer");
-    const loadFavIconContainer = document.getElementById("loadFavIconContainer");
-    const removeFavIconContainer = document.getElementById("removeFavIconContainer");
+document.addEventListener("DOMContentLoaded", async function() {
+  const qrCodeContainer = document.getElementById("qr-code-container");
+  const dotColor = document.getElementById("dotColor");
+  const dotType = document.getElementById("dotType");
+  const cornersSquareColor = document.getElementById("cornersSquareColor");
+  const cornersSquareType = document.getElementById("cornersSquareType");
+  const cornersDotColor = document.getElementById("cornersDotColor");
+  const cornersDotType = document.getElementById("cornersDotType");
+  const bgColor = document.getElementById("bgColor");
+  const imageUploadInput = document.getElementById("imageUpload");
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const qrData = urlParams.get("data");
+  console.log('Page URL: ', qrData);
+  const removeButtonContainer = document.getElementById("removeButtonContainer");
+  const removeLogoButton = document.getElementById("removeLogoButton");
+  const loadLogoContainer = document.getElementById("loadLogoContainer");
+  const addFaviconContainer = document.getElementById("addFaviconContainer");
+  const removeFaviconContainer = document.getElementById("removeFaviconContainer");
+  const addFaviconButton = document.getElementById("addFaviconButton");
+  const removeFaviconButton = document.getElementById("removeFaviconButton");
+  const qrDataUrl = new URL(qrData);
+  const domain = qrDataUrl.origin;
+  console.log('Domain: ', domain);
 
-    let qrCode = new QRCodeStyling({
-        data: qrData,
-    });
+  let qrCode = new QRCodeStyling({
+    data: qrData,
+  });
 
-    qrCode.append(qrCodeContainer);
-    
-	imageUploadInput.addEventListener("change", function () {
-    	const selectedFile = imageUploadInput.files[0];
-    	const selectedFileNameElement = document.getElementById("fileName");
+  qrCode.append(qrCodeContainer);
 
-    	// Update the selectedFileNameElement with the file name
-    	if (selectedFile) {
-        	selectedFileNameElement.textContent = `File: ${selectedFile.name}`; // Set the file name
-        	updateQRCode();
-        	removeButtonContainer.style.display = 'block'; // Show the remove logo button
-        	loadLogoContainer.style.display = 'none'; // Hide the add logo button
-    	} else {
-        	selectedFileNameElement.textContent = ''; // Clear the file name if no file is selected
-    	}
-	});
-	
-	removeLogoButton.addEventListener("click", function () {
-		imageUploadInput.value = ""; // Clear the selected file in the input
-		document.getElementById("fileName").textContent = ""; // Clear the displayed file name
-		removeButtonContainer.style.display = 'none'; // Hide the remove logo button
-		loadLogoContainer.style.display = 'block'; // Show the add logo button
-		updateQRCode(); // Update the QR Code without the image
-	});
+  imageUploadInput.addEventListener("change", function() {
+    const selectedFile = imageUploadInput.files[0];
+    const selectedFileNameElement = document.getElementById("fileName");
 
-
-    // Function to update the QR Code with the selected image and styling
-    function updateQRCode() {
-        const selectedFile = imageUploadInput.files[0];
-        const selectedFileNameElement = document.getElementById("fileName");
-        
-        qrCodeContainer.innerHTML = ''
-
-        if (selectedFile) {
-
-            const reader = new FileReader();
-            reader.onload = function (e) {
-                const imageDataUrl = e.target.result;
-                if (imageDataUrl) {
-                    qrCode = new QRCodeStyling({
-                        data: qrData,
-                        image: imageDataUrl, // Set the selected image as the logo
-                        imageOptions: {
-                            crossOrigin: "anonymous",
-                            margin: 5 // Enable cross-origin for the image if necessary
-                        },
-                        dotsOptions: {
-                        	color: dotColor.value,
-                        	type: dotType.value
-                        },
-                        cornersSquareOptions: {
-                        	color: cornersSquareColor.value,
-                        	type: cornersSquareType.value
-                        },
-                        cornersDotOptions: {
-                        	color: cornersDotColor.value,
-                        	type: cornersDotType.value
-                        },
-                        backgroundOptions: {
-                            color: bgColor.value,
-                        },
-                    });
-
-                    qrCode.append(qrCodeContainer); // Update the QR Code in the container
-                } else {
-                    console.error("Could not generate logo URL");
-                }
-            };
-
-            reader.readAsDataURL(selectedFile);
-        } else {
-             
-            qrCode = new QRCodeStyling({
-			data: qrData,
-			dotsOptions: {
-				color: dotColor.value,
-				type: dotType.value
-			},
-			cornersSquareOptions: {
-				color: cornersSquareColor.value,
-				type: cornersSquareType.value
-			},
-			cornersDotOptions: {
-				color: cornersDotColor.value,
-				type: cornersDotType.value
-			},
-			backgroundOptions: {
-				color: bgColor.value,
-			},
-		});
-
-		qrCode.append(qrCodeContainer); // Update the QR Code in the container
-		
-        }
+    // Update the selectedFileNameElement with the file name
+    if (selectedFile) {
+      selectedFileNameElement.textContent = `${selectedFile.name}`; // Set the file name
+      updateQRCode();
+      removeButtonContainer.style.display = 'block'; // Show the remove logo button
+      loadLogoContainer.style.display = 'none'; // Hide the add logo button
+      addFaviconContainer.style.display = 'block'; // Show add favicon button
+      removeFaviconContainer.style.display = 'none'; // Hide remove favicon button
+      document.getElementById('faviconName').textContent = ''; // Hide displayed favicon URL
+    } else {
+      selectedFileNameElement.textContent = ''; // Clear the file name if no file is selected
     }
-    
-    dotColor.addEventListener("change", updateQRCode);
-    bgColor.addEventListener("change", updateQRCode);
-    dotType.addEventListener("change", updateQRCode);
-    cornersSquareColor.addEventListener("change", updateQRCode);
-    cornersSquareType.addEventListener("change", updateQRCode);
-    cornersDotColor.addEventListener("change", updateQRCode);
-    cornersDotType.addEventListener("change", updateQRCode);
+  });
 
-    const downloadButton = document.getElementById("downloadButton");
-    downloadButton.addEventListener("click", function () {
-        if (!qrCode) {
-            console.error("No QR Code to download");
-            return;
+  removeLogoButton.addEventListener("click", function() {
+    imageUploadInput.value = ""; // Clear the selected file in the input
+    document.getElementById("fileName").textContent = ""; // Clear the displayed file name
+    removeButtonContainer.style.display = 'none'; // Hide the remove logo button
+    loadLogoContainer.style.display = 'block'; // Show the add logo button
+    updateQRCode(); // Update the QR Code without the image
+  });
+
+
+
+  async function getFaviconUrl() {
+    const parser = new DOMParser();
+    const response = await fetch(qrData);
+    if (!response.ok) {
+      throw new Error("Failed to fetch page");
+    }
+    const html = await response.text();
+    const page = parser.parseFromString(html, 'text/html');
+    const head = page.querySelector('head');
+    const links = head.querySelectorAll('link[rel~="icon"]');
+    return [...links].find(d => d.href.includes('favicon'));
+  }
+
+  addFaviconButton.addEventListener("click", async function() {
+    try {
+      const faviconUrl = await getFaviconUrl();
+      console.log('Favicon ID: ', faviconUrl);
+      let newFaviconUrl;
+      if (faviconUrl) {
+        console.log('Favicon URL: ', faviconUrl.href);
+        if (faviconUrl.href.startsWith('http')) {
+          newFaviconUrl = faviconUrl.href;
+          console.log('newFaviconUrl=', newFaviconUrl);
         } else {
-            const chooseFormat = document.getElementById('chooseFormat');
-            const format = chooseFormat.value;
-            qrCode.download({ name: "qrcode", extension: format });
+          const intFaviconUrl = faviconUrl.href;
+          const intFaviconUrl2 = intFaviconUrl.split('/');
+          const intFaviconUrl3 = intFaviconUrl2.slice(3).join('/');
+          newFaviconUrl = domain + '/' + intFaviconUrl3;
+          console.log('newFaviconUrl=', newFaviconUrl);
         }
-    });
-});
+        addFaviconContainer.style.display = 'none';
+        removeFaviconContainer.style.display = 'block';
+        imageUploadInput.value = ""; // Clear the selected file in the input
+        document.getElementById("fileName").textContent = ""; // Clear the displayed file name
+        removeButtonContainer.style.display = 'none'; // Hide the remove logo button
+        loadLogoContainer.style.display = 'block'; // Show the add logo button			
+      } else {
+        console.error('Favicon not found');
+        faviconName.textContent = 'Tab icon not found';
+      }
+      console.log('Final FaviconUrl=', newFaviconUrl);
+      updateQRCode(newFaviconUrl);
+    } catch (error) {
+      console.error('Error fetching favicon:', error);
+    }
+  });
 
-    
-    
-//     generateButton.addEventListener("click", function () {
-//         console.log('Generating...');
-//         const selectedDotColor = dotColor.value;
-//         const selectedBgColor = bgColor.value;
-//         const selectedFile = imageUploadInput.files[0];
-//         
-//         console.log('URL:', qrData);
-//         console.log('Foreground color:', selectedDotColor);
-//         console.log('Background color:', selectedBgColor);
-//         console.log('Logo:', selectedFile);
-//         
-//         qrCodeContainer.innerHTML = '';
-//         
-//         if (selectedFile) {
-// 			const reader = new FileReader();
-// 
-// 			reader.onload = function (e) {
-// 				const imageDataUrl = e.target.result;
-// 				if (imageDataUrl) {
-// 					console.log('Logo URL:', imageDataUrl);
-// 				} else {
-// 					console.error('Could not generate logo URL');
-// 				};
-// 				
-//         
-// 				const qrCode = new QRCodeStyling({
-// 					width: 300,
-// 					height: 300,
-// 					type: "canvas",
-// 					data: qrData,
-// 					image: imageDataUrl,
-// 					dotsOptions: {
-// 						color: selectedDotColor
-// 					},
-// 					backgroundOptions: {
-// 						color: selectedBgColor
-// 					},
-// 					imageOptions: {
-// 						margin: 2
-// 					}
-// 				});
-// 				
-// 				qrCode.append(qrCodeContainer);
-// 				qrCode.download({ name: 'qr', extension: 'png'});
-// 			};
-// 			
-// 			reader.readAsDataURL(selectedFile);
-// 		
-// 		} else {
-// 		
-// 				const qrCode = new QRCodeStyling({
-// 					width: 300,
-// 					height: 300,
-// 					type: "canvas",
-// 					data: qrData,
-// 					dotsOptions: {
-// 						color: selectedDotColor
-// 					},
-// 					backgroundOptions: {
-// 						color: selectedBgColor
-// 					},
-// 					imageOptions: {
-// 						margin: 20
-// 					}
-// 				});
-// 				
-// 				qrCode.append(qrCodeContainer);
-// 				qrCode.download({ name: 'qr', extension: 'png'});		
-// 		console.error ('No selected file')
-// 		};
-//         
-// 
-//     });
-// });
+  removeFaviconButton.addEventListener('click', function() {
+    addFaviconContainer.style.display = 'block'; // Show add favicon button
+    removeFaviconContainer.style.display = 'none'; // Hide remove favicon button
+    updateQRCode(); // Update the QR Code without the favicon
+  });
+
+
+
+  // Function to update the QR Code with the selected image and styling
+  function updateQRCode(newFaviconUrl) {
+    const selectedFile = imageUploadInput.files[0];
+    const selectedFileNameElement = document.getElementById("fileName");
+
+    qrCodeContainer.innerHTML = ''
+
+    if (newFaviconUrl) {
+      console.log('Loading Favicon: ', newFaviconUrl);
+      qrCode = new QRCodeStyling({
+        data: qrData,
+        image: newFaviconUrl,
+        imageOptions: {
+          crossOrigin: "anonymous",
+          margin: 5
+        },
+        dotsOptions: {
+          color: dotColor.value,
+          type: dotType.value
+        },
+        cornersSquareOptions: {
+          color: cornersSquareColor.value,
+          type: cornersSquareType.value
+        },
+        cornersDotOptions: {
+          color: cornersDotColor.value,
+          type: cornersDotType.value
+        },
+        backgroundOptions: {
+          color: bgColor.value,
+        },
+      });
+
+      qrCode.append(qrCodeContainer); // Update the QR Code in the container
+
+    } else if (selectedFile) {
+
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        const imageDataUrl = e.target.result;
+        if (imageDataUrl) {
+          qrCode = new QRCodeStyling({
+            data: qrData,
+            image: imageDataUrl, // Set the selected image as the logo
+            imageOptions: {
+              crossOrigin: "anonymous",
+              margin: 5 // Enable cross-origin for the image if necessary
+            },
+            dotsOptions: {
+              color: dotColor.value,
+              type: dotType.value
+            },
+            cornersSquareOptions: {
+              color: cornersSquareColor.value,
+              type: cornersSquareType.value
+            },
+            cornersDotOptions: {
+              color: cornersDotColor.value,
+              type: cornersDotType.value
+            },
+            backgroundOptions: {
+              color: bgColor.value,
+            },
+          });
+
+          qrCode.append(qrCodeContainer); // Update the QR Code in the container
+        } else {
+          console.error("Could not generate logo URL");
+        }
+      };
+
+      reader.readAsDataURL(selectedFile);
+    } else {
+
+      qrCode = new QRCodeStyling({
+        data: qrData,
+        dotsOptions: {
+          color: dotColor.value,
+          type: dotType.value
+        },
+        cornersSquareOptions: {
+          color: cornersSquareColor.value,
+          type: cornersSquareType.value
+        },
+        cornersDotOptions: {
+          color: cornersDotColor.value,
+          type: cornersDotType.value
+        },
+        backgroundOptions: {
+          color: bgColor.value,
+        },
+      });
+
+      qrCode.append(qrCodeContainer); // Update the QR Code in the container
+
+    }
+  }
+
+  function configureQRCode() {
+    qrCode.update({
+      data: qrData,
+      dotsOptions: {
+        color: dotColor.value,
+        type: dotType.value,
+      },
+      cornersSquareOptions: {
+        color: cornersSquareColor.value,
+        type: cornersSquareType.value,
+      },
+      cornersDotOptions: {
+        color: cornersDotColor.value,
+        type: cornersDotType.value,
+      },
+      backgroundOptions: {
+        color: bgColor.value,
+      },
+    });
+  }
+
+
+  dotColor.addEventListener("change", configureQRCode);
+  bgColor.addEventListener("change", configureQRCode);
+  dotType.addEventListener("change", configureQRCode);
+  cornersSquareColor.addEventListener("change", configureQRCode);
+  cornersSquareType.addEventListener("change", configureQRCode);
+  cornersDotColor.addEventListener("change", configureQRCode);
+  cornersDotType.addEventListener("change", configureQRCode);
+
+  const downloadButton = document.getElementById("downloadButton");
+  downloadButton.addEventListener("click", function() {
+    if (!qrCode) {
+      console.error("No QR Code to download");
+      return;
+    } else {
+      const chooseFormat = document.getElementById('chooseFormat');
+      const format = chooseFormat.value;
+      qrCode.download({
+        name: "qrcode",
+        extension: format
+      });
+    }
+  });
+});
