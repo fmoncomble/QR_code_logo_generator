33a96eaa7f1624d1a18652eb9d8c6803f2e060de
fmoncomble <florent.moncomble@gmail.com>
2023-12-11 19:55:43 +0100

    Updated Chrome version

--- chrome/content.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("error", function(event) {
-if (event.error && event.error.message && event.error.message.includes('CORS policy')) {
-	alert('Error: inaccessible resource');
-	imageUrlLoader.value = "";
-	updateQRCode();
-}
-});
